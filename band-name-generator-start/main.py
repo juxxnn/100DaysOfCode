c1189906dbd8d51ed411283eba51381f6d555398
juxxnn <julianastankovic@gmail.com>
2023-10-02 19:47:55 -0300

    Day 1

---
+++ band-name-generator-start/main.py
@@ -0,0 +1,11 @@
+# Imprime uma mensagem de boas-vindas para o usuário.
+print("Welcome to the Band Name Generator.")
+
+# Solicita ao usuário que insira o nome da cidade em que cresceu e armazena a entrada na variável 'city'.
+city = input("What's name of the city you grew up in?\n")
+
+# Solicita ao usuário que insira o nome do seu animal de estimação e armazena a entrada na variável 'pet_name'.
+pet_name = input("What's your pet's name?\n")
+
+# Combina o nome da cidade e o nome do animal de estimação para criar um nome de banda e imprime-o na tela.
+print("Your band name could be " + city + " " + pet_name)
